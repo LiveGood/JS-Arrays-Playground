d231b0bb6699e8706953d69768a92e1bc35c3ced
LiveGood <FleshThps@gmail.com>
2020-10-22 22:14:49 +0300

    added method some

---
+++ customSome.js
@@ -0,0 +1,13 @@
+const contextProvider = require("./helpers/contextProvider");
+
+const customSome = function(arr, cb, thisArg) {
+  cb = contextProvider(arr, cb, thisArg)
+
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i], i)) return true;
+  }
+
+  return false;
+}
+
+module.exports = customSome
